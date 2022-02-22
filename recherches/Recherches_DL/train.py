f1521a6043762c5d953d650d3344501f420276e4
LangloisMattéo <langlois.matteo49@gmail.com>
2022-02-22 20:44:45 +0100

    Change the architecture of project, and add a README.md

---
+++ recherches/Recherches_DL/train.py
@@ -0,0 +1,110 @@
+import tensorflow as tf
+
+# Helper libraries
+import numpy as np
+import matplotlib.pyplot as plt
+import os
+
+gpus = tf.config.experimental.list_physical_devices('GPU')
+tf.config.experimental.set_memory_growth(gpus[0], True)
+
+config = tf.compat.v1.ConfigProto(gpu_options=tf.compat.v1.GPUOptions(allow_growth=True))
+sess = tf.compat.v1.Session(config=config)
+
+IMAGE_SHAPE = (224, 224)
+TRAINING_DATA_DIR = 'D:/mamac/Documents/Cours/Polytech/Peip2/ProjetS4/testDL/dataset/train/'
+VALID_DATA_DIR = 'D:/mamac/Documents/Cours/Polytech/Peip2/ProjetS4/testDL/dataset/valid/'
+
+datagen = tf.keras.preprocessing.image.ImageDataGenerator(
+    rescale=1. / 255
+)
+train_generator = datagen.flow_from_directory(
+    TRAINING_DATA_DIR,
+    shuffle=True,
+    target_size=IMAGE_SHAPE,
+)
+valid_generator = datagen.flow_from_directory(
+    VALID_DATA_DIR,
+    shuffle=False,
+    target_size=IMAGE_SHAPE,
+)
+
+
+def build_model(num_classes):
+    modelB = tf.keras.Sequential([
+        tf.keras.layers.Conv2D(filters=8, kernel_size=(3, 3), activation='relu', input_shape=(224, 224, 3)),
+        tf.keras.layers.MaxPool2D(pool_size=(2, 2)),
+        tf.keras.layers.Conv2D(filters=16, kernel_size=(3, 3), activation='relu'),
+        tf.keras.layers.MaxPool2D(pool_size=(2, 2)),
+        tf.keras.layers.Conv2D(filters=32, kernel_size=(3, 3), activation='relu'),
+        tf.keras.layers.MaxPool2D(pool_size=(2, 2)),
+        tf.keras.layers.Flatten(),
+        tf.keras.layers.Dense(64, activation='relu'),
+        tf.keras.layers.Dense(num_classes, activation='softmax')
+    ])
+    return modelB
+
+
+model = build_model(num_classes=3)
+
+model.compile(
+    optimizer=tf.keras.optimizers.Adam(learning_rate=0.0001),
+    loss=tf.keras.losses.CategoricalCrossentropy(from_logits=True),
+    metrics=['accuracy']
+)
+
+print(model.summary())
+
+EPOCHS = 20
+BATCH_SIZE = 32
+history = model.fit(train_generator,
+                    steps_per_epoch=train_generator.samples // BATCH_SIZE,
+                    epochs=EPOCHS,
+                    validation_data=valid_generator,
+                    validation_steps=valid_generator.samples // BATCH_SIZE,
+                    verbose=1
+                    )
+
+train_loss = history.history['loss']
+train_acc = history.history['accuracy']
+valid_loss = history.history['val_loss']
+valid_acc = history.history['val_accuracy']
+
+
+def save_plots(train_acc, valid_acc, train_loss, valid_loss):
+    """
+    Function to save the loss and accuracy plots to disk.
+    """
+    # accuracy plots
+    plt.figure(figsize=(12, 9))
+    plt.plot(
+        train_acc, color='green', linestyle='-',
+        label='train accuracy'
+    )
+    plt.plot(
+        valid_acc, color='blue', linestyle='-',
+        label='validataion accuracy'
+    )
+    plt.xlabel('Epochs')
+    plt.ylabel('Accuracy')
+    plt.legend()
+    plt.savefig('accuracy.png')
+    plt.show()
+    # loss plots
+    plt.figure(figsize=(12, 9))
+    plt.plot(
+        train_loss, color='orange', linestyle='-',
+        label='train loss'
+    )
+    plt.plot(
+        valid_loss, color='red', linestyle='-',
+        label='validataion loss'
+    )
+    plt.xlabel('Epochs')
+    plt.ylabel('Loss')
+    plt.legend()
+    plt.savefig('loss.png')
+    plt.show()
+
+
+save_plots(train_acc, valid_acc, train_loss, valid_loss)
