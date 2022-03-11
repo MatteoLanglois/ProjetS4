2f6b01d5a2302af2aa6feda154c0660c2fdb39ec
matteolanglois <langlois.matteo49@gmail.com>
2022-03-11 15:38:51 +0100

    Use Haar for some files and video

--- recherches/Recherches_FeatureExtraction/TestHaar_Recog.py
+++ recherches/Recherches_FeatureExtraction/TestHaar_Recog.py
@@ -1,32 +1,39 @@
 import matplotlib.pyplot as plt
 import cv2
 
+
+def FaceRecog(img, face_cascade, eye_cascade, smile_cascade):
+    gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
+    faces = face_cascade.detectMultiScale(gray, 1.3, 5)
+    for (x, y, w, h) in faces:
+        cv2.rectangle(img, (x, y), (x + w, y + h), (255, 0, 0), 2)
+        roi_gray = gray[y:y + h, x:x + w]
+        roi_color = img[y:y + h, x:x + w]
+        eyes = eye_cascade.detectMultiScale(roi_gray)
+        for (ex, ey, ew, eh) in eyes:
+            cv2.rectangle(roi_color, (ex, ey), (ex + ew, ey + eh), (0, 255, 0), 2)
+        smiles = smile_cascade.detectMultiScale(roi_gray)
+        for (sx, sy, sw, sh) in smiles:
+            cv2.rectangle(roi_color, (sx, sy), (sx + sw, sy + sh), (0, 0, 255), 2)
+    return img
+
+
 img_with = [cv2.imread(f'ProjetS4/dataset/train/with_mask/with_mask_{I}.jpg') for I in range(0, 2)]
 img_without = [cv2.imread(f'ProjetS4/dataset/train/without_mask/without_mask_{I}.jpg') for I in range(0, 2)]
 img_incorrect = [cv2.imread(f'ProjetS4/dataset/train/incorrect_mask/incorrect_mask_{I}.jpg') for I in [0, 3]]
-harr_cascade = cv2.CascadeClassifier("ProjetS4/recherches/Recherches_FeatureExtraction/harr_face_detect_classifier.xml")
+face_cascade = cv2.CascadeClassifier(cv2.data.haarcascades + 'haarcascade_frontalface_default.xml')
+eye_cascade = cv2.CascadeClassifier(cv2.data.haarcascades + 'haarcascade_eye.xml')
+smile_cascade = cv2.CascadeClassifier(cv2.data.haarcascades + "haarcascade_smile.xml")
 
 img_with_edit = []
 img_without_edit = []
 img_incorrect_edit = []
 
+plt.axis("off")
 for I in range(len(img_with)):
-    img_with_edit.append(cv2.resize(cv2.cvtColor(img_with[I], cv2.COLOR_BGR2GRAY), (600, 900), interpolation=cv2.INTER_CUBIC))
-    img_without_edit.append(cv2.resize(cv2.cvtColor(img_without[I], cv2.COLOR_BGR2GRAY), (600, 900), interpolation=cv2.INTER_CUBIC))
-    img_incorrect_edit.append(cv2.resize(cv2.cvtColor(img_incorrect[I], cv2.COLOR_BGR2GRAY), (600, 900), interpolation=cv2.INTER_CUBIC))
-
-    face_cords_with = harr_cascade.detectMultiScale(img_with_edit[I], scaleFactor=1.1, minNeighbors=1)
-    face_cords_without = harr_cascade.detectMultiScale(img_without_edit[I], scaleFactor=1.1, minNeighbors=1)
-    face_cords_incorrect = harr_cascade.detectMultiScale(img_incorrect_edit[I], scaleFactor=1.1, minNeighbors=1)
-
-    for x, y, w, h in face_cords_with:
-        cv2.rectangle(img_with[I], (x, y), (x + w, y + h), (255, 0, 0), thickness=2)
-
-    for x, y, w, h in face_cords_without:
-        cv2.rectangle(img_without[I], (x, y), (x + w, y + h), (255, 0, 0), thickness=2)
-
-    for x, y, w, h in face_cords_incorrect:
-        cv2.rectangle(img_incorrect[I], (x, y), (x + w, y + h), (255, 0, 0), thickness=2)
+    img_with_edit = FaceRecog(img_with[I], face_cascade, eye_cascade, smile_cascade)
+    img_without_edit = FaceRecog(img_without[I], face_cascade, eye_cascade, smile_cascade)
+    img_incorrect_edit = FaceRecog(img_incorrect[I], face_cascade, eye_cascade, smile_cascade)
 
     plt.subplot(3, 3, I + 1)
     plt.imshow(cv2.cvtColor(img_with[I], cv2.COLOR_RGB2BGR))
--- recherches/Recherches_FeatureExtraction/haarcascades.py
+++ recherches/Recherches_FeatureExtraction/haarcascades.py
@@ -1,7 +1,7 @@
 import sys
 import cv2
 
-x=y=w=h=0
+x = y = w = h = 0
 
 # Get user supplied values
 imagePath = sys.argv[1]
@@ -15,11 +15,11 @@ image = cv2.imread("projets4/dataset/test/incorrect_mask/incorrect_mask_1.jpg")
 gray = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
 
 # Draw a rectangle around the faces
-roi_gray = gray[y:y+h, x:x+w]
-roi_color = image[y:y+h, x:x+w]
+roi_gray = gray[y:y + h, x:x + w]
+roi_color = image[y:y + h, x:x + w]
 nose = noseCascade.detectMultiScale(roi_gray)
 
-for (ex,ey,ew,eh) in nose:
-    cv2.rectangle(roi_color,(ex,ey),(ex+ew,ey+eh),(0,255,0),2)
+for (ex, ey, ew, eh) in nose:
+    cv2.rectangle(roi_color, (ex, ey), (ex + ew, ey + eh), (0, 255, 0), 2)
 
 cv2.waitKey(0)
