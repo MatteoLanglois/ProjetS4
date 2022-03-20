aabe45a60e01a7177b7f44ba5c6f2197726c10df
matteolanglois <langlois.matteo49@gmail.com>
2022-03-20 13:51:11 +0100

    Try to correct a problem on VideoMaskDetection.py, but not resolve for now

--- DeepLearning/VideoMaskDetection.py
+++ DeepLearning/VideoMaskDetection.py
@@ -2,17 +2,12 @@ import cv2
 import imutils
 import tensorflow as tf
 import numpy as np
+import matplotlib.pyplot as plt
 
 IMAGE_SHAPE = (224, 224)
 TRAINING_DATA_DIR = './dataset/train/'
 
-train_ds = tf.keras.utils.image_dataset_from_directory(
-    TRAINING_DATA_DIR,
-    validation_split=0.2,
-    subset="training",
-    seed=123,
-    image_size=IMAGE_SHAPE,
-    batch_size=32)
+train_ds = tf.keras.utils.image_dataset_from_directory(TRAINING_DATA_DIR)
 
 class_names = train_ds.class_names
 
@@ -20,22 +15,31 @@ model = tf.keras.models.load_model('./Deeplearning/saved_model/modelClean')
 
 
 def MaskDetection(frame):
-    frame = imutils.resize(frame, width=224)
+    # Prétraitement de l'image
+    frame = cv2.resize(frame, IMAGE_SHAPE, interpolation=cv2.INTER_CUBIC)
     img_array = tf.keras.utils.img_to_array(frame)
     img_array = tf.expand_dims(img_array, 0)
+    # Prédiction de la classe de l'image
     prediction = model.predict(img_array)
-    class_name = class_names[np.argmax(prediction[0])]
+    print(prediction)
+    # Récupération de la classe prédite
+    class_name = class_names[np.argmax(tf.nn.softmax(prediction[0]))]
     return class_name
 
 
 cam = cv2.VideoCapture(0, cv2.CAP_DSHOW)
 
 while True:
-    ret_val, img = cam.read()
-    cv2.imshow('Webcam', img)
-    img = cv2.resize(img, IMAGE_SHAPE)
+    _, img = cam.read()
+    gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
+    face_cascade = cv2.CascadeClassifier(cv2.data.haarcascades + 'haarcascade_frontalface_default.xml')
+    faces = face_cascade.detectMultiScale(gray, 1.3, 5)
+    for (x, y, w, h) in faces:
+        cv2.rectangle(img, (x, y), (x + w, y + h), (255, 255, 0), 2)
     predic = MaskDetection(cv2.cvtColor(img, cv2.COLOR_RGB2BGR))
-    print(f"-------\n{predic}\n-------")
+    cv2.putText(img, predic, (10, 30), cv2.FONT_HERSHEY_SIMPLEX, 1, (0, 0, 255), 2)
+    cv2.imshow('Webcam', img)
     if cv2.waitKey(1) == 27:
         break  # esc to quit
 cv2.destroyAllWindows()
+plt.show()
